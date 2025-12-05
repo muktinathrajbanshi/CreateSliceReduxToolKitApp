7fd3206ed4127d4651c0d3ae9121f3b7f767fbd5
unknown <muktinathrajbanshi786@gmail.com>
2025-12-05 10:35:13 +0530

    now this is the actual challenge for doing so

--- src/store.jsx
+++ src/store.jsx
@@ -1,71 +1,28 @@
 import { configureStore, createSlice } from "@reduxjs/toolkit";
 
 /* eslint-disable no-case-declarations */
-const ADD_TASK = "task/add";
-const DELETE_TASK = "task/delete";
-const FETCH_TASKS = "task/fetch";
+// const ADD_TASK = "task/add";
+// const DELETE_TASK = "task/delete";
+// const FETCH_TASKS = "task/fetch";
 
 
 const initialState = {
     task: [],
-    isLoading: false,
 };
 
-// Step 5: Create action creators
-export const addTask = (data) => {
-   return { type: ADD_TASK, payload: data }
-}; 
-
-export const deleteTask = (id) => {
-    return { type: DELETE_TASK, payload: id }
-};
-
-export const fetchTask = () => {
-    return async (dispatch) => {
-        try {
-            const res = await fetch(
-                "https://jsonplaceholder.typicode.com/todos?_limit=3"
-            );
-            const task = await res.json();
-            console.log(task);
-            
-            dispatch({ 
-                type: FETCH_TASKS, 
-                payload: task.map((curTask) => curTask.title),
-             });
-        } catch (error) {
-            console.log(error);
-        }
-    }
-};
+//? RTK slice
+const taskReducer = createSlice({
+    name: "task",
+    initialState,
+    reducers:{
+        addTask(state, action) {},
+        deleteTask(state, action) {},
+    },
+});
 
 
-// Step 1: Create a simple reducer function 
-const taskReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case ADD_TASK:
-            return {
-                ... state,
-                task: [... state.task, action.payload],
-            };
 
-        case DELETE_TASK:
-            const updatedTask = state.task.filter((curTask, index) => index !== action.payload); 
-               return {
-                ...state,
-                task: updatedTask,
-               };
-
-        case FETCH_TASKS: 
-            return {
-                ...state,
-                task:[... state.task, ...action.payload],
-            }       
-        default:
-            return state;
-    }
-};
-
+//! New Style
 export const store = configureStore({
     reducer: {
         taskReducer,
