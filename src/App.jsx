fe0b3c25f33cabf07dd0721ece4c35fd6f80d475
unknown <muktinathrajbanshi786@gmail.com>
2025-12-06 04:26:40 +0200

    new structure is created

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 import { Todo } from './components/Todo';
 
 const App = () => {
-  return <Todo />;
+  return (
+    <div className="container">
+      <div className="todo-app">
+        <h1>To-do List:</h1>
+        <div className="row">
+          <form>
+            <input type="text" id="input-box" placeholder="Add a new Task" />
+            <button type="submit">Add Task</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  )
 };
 
 export default App;
